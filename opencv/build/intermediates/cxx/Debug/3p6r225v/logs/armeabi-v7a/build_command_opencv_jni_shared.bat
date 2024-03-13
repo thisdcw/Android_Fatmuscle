a181d9de68c8f9feab0fe8741bb3c91d44cf7168
thisdcw <thisdcw@outlook.com>
2024-03-13 16:11:48 +0800

    上传opencv库

---
+++ opencv/build/intermediates/cxx/Debug/3p6r225v/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"D:\\Program Files\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Project\\Android Project\\Android_Fatmuscle\\opencv\\.cxx\\Debug\\3p6r225v\\armeabi-v7a" ^
+  opencv_jni_shared
